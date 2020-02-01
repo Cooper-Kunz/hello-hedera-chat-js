dfabea1c45637f3f9dcfd8598f79097cc88df96e
Cooper Kunz <cooperakunz@gmail.com>
2020-01-31 17:21:52 -0700

    match process.env to setup instructions

--- hedera-client.js
+++ hedera-client.js
@@ -4,8 +4,8 @@ const { Client } = require("@hashgraph/sdk");
 const HederaClient = new Client({
   network: { "0.testnet.hedera.com:50211": "0.0.3" },
   operator: {
-    account: process.env.testID,
-    privateKey: process.env.testPrivateKey
+    account: process.env.ACCOUNT_ID,
+    privateKey: process.env.PRIVATE_KEY
   }
 });
 
